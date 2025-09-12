7125ea74de80f7a77fd23d7fdee873aa9bfe2466
Jerem16 <jeremy.lemaignent@gmail.com>
2025-09-12 11:56:29 +0200

    SAVE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,7 @@
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import HeaderLazy from "../src/components/header/HeaderLazy";
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
-const Montserrat = localFont({
-    src: "./fonts/Montserrat.woff2",
-    variable: "--montserrat",
-    weight: "100 900",
-    display: "swap",
-});
-export const Roboto = localFont({
-    src: "/fonts/Roboto.woff2",
-    variable: "--Roboto",
-    weight: "700",
-    display: "swap",
-});
-const Nunito = localFont({
-    src: "./fonts/Nunito.woff2",
-    variable: "--nunito",
-    weight: "400",
-    display: "swap",
-});
 
 export default function RootLayout({
     children,
@@ -30,9 +12,23 @@ export default function RootLayout({
         <html lang="fr-FR">
             <head>
                 <link rel="preload" href="/img/retroviseur.svg" as="image" />
+
+                {/* <link rel="preload" href="/css/globals.css" as="style" />
+                <link
+                    rel="stylesheet"
+                    href="/css/globals.css"
+                    fetchPriority="high"
+                /> */}
+
+                {/* <link rel="preload" href="/css/mobileDefer.css" as="style" />
+                <link
+                    rel="stylesheet"
+                    href="/css/mobileDefer.css"
+                    fetchPriority="low"
+                /> */}
             </head>
             <body
-                className={`${Montserrat.variable} ${Roboto.variable} ${Nunito.variable}`}
+                // className={`${Montserrat.variable} ${Roboto.variable} ${Nunito.variable}`}
                 id="top"
             >
                 {/* <DesktopRedirect /> */}
