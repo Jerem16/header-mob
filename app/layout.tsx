a4268899a7a5de555ee662ba610ec207b2bd5498
Jerem16 <jeremy.lemaignent@gmail.com>
2025-09-12 13:47:19 +0200

    SAVE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,25 @@
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import HeaderLazy from "../src/components/header/HeaderLazy";
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
-
+const Montserrat = localFont({
+    src: "./fonts/Montserrat.woff2",
+    variable: "--montserrat",
+    weight: "100 900",
+    display: "swap",
+});
+export const Roboto = localFont({
+    src: "/fonts/Roboto.woff2",
+    variable: "--Roboto",
+    weight: "700",
+    display: "swap",
+});
+const Nunito = localFont({
+    src: "./fonts/Nunito.woff2",
+    variable: "--nunito",
+    weight: "400",
+    display: "swap",
+});
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -28,7 +45,7 @@ export default function RootLayout({
                 /> */}
             </head>
             <body
-                // className={`${Montserrat.variable} ${Roboto.variable} ${Nunito.variable}`}
+                className={`${Montserrat.variable} ${Roboto.variable} ${Nunito.variable}`}
                 id="top"
             >
                 {/* <DesktopRedirect /> */}
