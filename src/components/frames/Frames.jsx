b629c13132a3245885697d773cd3d8919d8f6984
Jerem16 <jeremy.lemaignent@gmail.com>
2025-09-13 18:42:26 +0200

    SAVE

--- src/components/frames/Frames.jsx
+++ src/components/frames/Frames.jsx
@@ -5,6 +5,8 @@ const Frames = ({ className, id, children, ...rest }) => {
     return (
         <section className={className} id={id} {...rest}>
             {children}
+            <div className="fixed-menu"></div>
+            <h2>Welcome !</h2>
             <p>Hello, World !</p>
         </section>
     );
