482d8dfd35400cc4590f3d9bdb3edc3ca8ecceb7
Jérémy Lemaignent <94492716+Jerem16@users.noreply.github.com>
2025-09-15 08:43:05 +0200

    fix: remove content data

--- src/assets/data/content/services.ts
+++
@@ -1,8 +0,0 @@
-import { ServiceContent } from "../interfaces/content";
-
-export const serviceContent: ServiceContent[] = [
-    {
-        description:
-            "Découvre un aperçu de mes services conçus spécialement pour répondre aux besoins des conducteurs débutants et confirmés : ",
-    },
-];
